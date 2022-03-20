55fa849d58efc5a9bb005cdf4b244d5dc41248c0
Pierre-Lctx <pierre.lechatreux@hotmail.fr>
2022-03-20 21:07:40 +0100

    commit

---
+++ VoxelGenerator/Assets/Scripts/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    foreward,   //z+ direction
+    right,      //x+ direction
+    backwards,  //z- direction
+    left,       //x- direction
+    up,         //y+ direction
+    down        //y- direction
+}
---
+++ VoxelGenerator/Assets/Scripts/DirectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DirectionExtensions
+{
+    public static Vector3Int GetVector(this Direction direction)
+    {
+        return Vector3Int.RoundToInt(GetTheVector(direction));
+    }
+
+    public static Vector3 GetTheVector(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.up:
+                return Vector3.up;
+            case Direction.down:
+                return Vector3.down;
+            case Direction.right:
+                return Vector3.right;
+            case Direction.left:
+                return Vector3.left;
+            case Direction.foreward:
+                return Vector3.forward;
+            case Direction.backwards:
+                return Vector3.back;
+            default:
+                throw new Exception("Invalid input direction");
+        }
+    }
+}
